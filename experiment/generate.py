9fa78ffcfd294abf18ae44dd94ce93380096989e
tc-imba <liuyh615@sjtu.edu.cn>
2021-02-21 21:38:09 +0800

    prepare experiment

--- experiment/generate.py
+++ experiment/generate.py
@@ -6,8 +6,8 @@ project_root = os.path.dirname(os.path.dirname(__file__))
 program = os.path.join(project_root, "cmake-build-release", "MAPF-generate-well-formed-new")
 
 
-def generate(x, y, agent, agent_per_task, seed, release=False):
-    for seed in range(10,20):
+def generate(x, y, agent, agent_per_task, release=False):
+    for seed in range(0, 10):
         args = [
             program,
             "-x", str(x),
@@ -26,11 +26,11 @@ def main():
     os.chdir(project_root)
     for m in [10, 20, 30, 40, 50]:
         for k in [2, 5, 10]:
-            generate(5, 2, m, k, True)
+            # generate(5, 2, m, k, True)
             generate(5, 2, m, k, False)
     for m in [60, 90, 120, 150, 180]:
         for k in [2, 5, 10]:
-            generate(8, 3, m, k, True)
+            # generate(8, 3, m, k, True)
             generate(8, 3, m, k, False)
     # generate(8, 3, 60, 10, False)
 
